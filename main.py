e36c5d1a84391c7336e6319e7889a9ff711d47d7
Hugh Meade <hughtmeade@gmail.com>
2025-06-17 19:27:15 +1000

    Updated values code can get values needed and stored in comments

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pyautogui
 import keyboard
 
+#Poses for seeds buttons: (630, 430), (630, 560)
+#Can buy colours, (29, 179, 29)
+
 print(pyautogui.size())
 
 def get_values():
 
     pos = pyautogui.position()
-    print(pos, pyautogui.pixel(pos))
+    print(pos, pyautogui.pixel(pos[0], pos[1]))
 
-keyboard.add_hotkey("q", get_values())
+keyboard.add_hotkey("q", get_values)
 
 keyboard.wait("esc")
